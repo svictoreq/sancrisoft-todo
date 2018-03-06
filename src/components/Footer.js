3f1d8835239ad7584811149eb060a8c1c65b809a
Victor Requena <svictoreq@gmail.com>
2018-03-06 14:32:32 -0400

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import ClearCompleted from '../containers/ClearCompleted';
 
 const Footer = (props) => {
 	return(
-		<section id="filters">
+		<footer>
 			<div id="filters">
 				<Filters filter="ALL" />
 				<Filters filter="ACTIVE" />
 				<Filters filter="COMPLETED" />
 			</div>
 			<ClearCompleted />
-		</section>
+		</footer>
 	)
 }
 
